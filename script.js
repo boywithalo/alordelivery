a39e949466588f56da806e507a4c333d97fa45ff
bwh <113823007+boywithalo@users.noreply.github.com>
2024-09-20 21:30:20 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menubutton = document.querySelector('.menu');
+let header = document.querySelector('.header');
+
+menubutton.addEventListener('click', menutoggle);
+
+function menutoggle() {
+    header.classList.toggle('opened');
+}
+
+
+
+window.addEventListener('load', function() {
+    let preloader = document.getElementById('preloader');
+    
+    // Add a 2-second delay after the page has fully loaded
+    setTimeout(function() {
+        preloader.classList.add('fade-out'); // Add the fade-out class after delay
+        
+        // Remove the preloader after the fade-out transition
+        setTimeout(function() {
+            preloader.style.display = 'none';
+        }, 1000); // 1-second match with the CSS transition duration
+        
+    }, 2000); // 2-second delay before starting the fade-out
+});
